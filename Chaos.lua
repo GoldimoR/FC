b6e70785d4035ced28ceceec6f234abaf831c5fd
GoldimoR <waldemar1995i@gmail.com>
2018-11-12 18:24:50 +0200

    Add files via upload

---
+++ Chaos.lua
@@ -0,0 +1,223 @@
+local chaos = {}
+
+chaos.optionEnableScript = Menu.AddOption({"Hero by Rasta", "Chaos Knigth"}, "1.Enable", "Turn on/off this script.")
+chaos.PressComboKey = Menu.AddKeyOption({ "Hero by Rasta","Chaos Knigth" }, "2. Execute combo key", Enum.ButtonCode.KEY_I)
+local optionAwareness = Menu.AddOption({"Hero by Rasta", "Chaos Knigth"}, "Awareness", "Show how many hits left to kill enemy")
+local optionKillSteal = Menu.AddOption({"Hero by Rasta", "Chaos Knigth"}, "Kill Steal", "Auto cast RiFt")
+local font = Renderer.LoadFont("Tahoma", 30, Enum.FontWeight.EXTRABOLD)
+chaos.npcName = "npc_dota_hero_chaos_knight"
+
+function chaos.OnUpdate()
+	if not Menu.IsEnabled(chaos.optionEnableScript) then return true end
+    local myHero = Heroes.GetLocal()
+	if not myHero then return end
+	if NPC.GetUnitName(myHero) ~= chaos.npcName then return end
+	
+    if Menu.IsEnabled(optionKillSteal) then
+    chaos.KillSteal(myHero)
+	end
+	
+	
+	if Menu.IsKeyDown(chaos.PressComboKey) then	
+		-- Skills
+		local rift = NPC.GetAbility(myHero, "chaos_knight_reality_rift")
+		local bolt = NPC.GetAbility(myHero, "chaos_knight_chaos_bolt") 
+		local phantasm = NPC.GetAbility(myHero, "chaos_knight_phantasm") 
+
+		
+		-- Items
+		local MoM = NPC.GetItem(myHero, "item_mask_of_madness", true)
+		local manta = NPC.GetItem(myHero, "item_manta", true)
+		local mjolnir = NPC.GetItem(myHero, "item_mjollnir", true)
+		local lotus = NPC.GetItem(myHero, "item_lotus_orb", true)
+		local BKB = NPC.GetItem(myHero, "item_black_king_bar", true)
+		local blademail = NPC.GetItem(myHero, "item_blade_mail", true)
+		local diffusal = NPC.GetItem(myHero, "item_diffusal_blade", true)
+		local nullifier = NPC.GetItem(myHero, "item_nullifier", true)
+		local medallion = NPC.GetItem(myHero, "item_medallion_of_courage", true)
+		local solarCrest = NPC.GetItem(myHero, "item_solar_crest", true)
+		local aghanimScepter = NPC.GetItem(myHero, "item_ultimate_scepter", true)
+		local orchid = NPC.GetItem(myHero, "item_orchid", true)
+		local bloodthorn = NPC.GetItem(myHero, "item_bloodthorn", true)
+		local abyssalblade = NPC.GetItem(myHero, "item_abyssal_blade", true)
+		local forceStaff = NPC.GetItem(myHero, "item_force_staff", true)
+		local hurricanePike = NPC.GetItem(myHero, "item_hurricane_pike", true)
+		local eul = NPC.GetItem(myHero, "item_cyclone", true)
+		local Blink = NPC.GetItem(myHero, "item_blink", true)
+		
+		local myMana = NPC.GetMana(myHero)
+		local enemyTarget = Input.GetNearestHeroToCursor(Entity.GetTeamNum(myHero), Enum.TeamType.TEAM_ENEMY)
+		
+		if enemyTarget and NPC.HasModifier(enemyTarget, "modifier_item_ultimate_scepter") and Ability.IsReady(NPC.GetAbility(enemyTarget, "antimage_spell_shield")) then
+			
+
+			if diffusal and Ability.IsReady(diffusal) and Ability.IsCastable(diffusal, myMana) then
+				Ability.CastTarget(diffusal, enemyTarget) 
+			return end
+			
+			return 
+		end
+		
+		if enemyTarget and NPC.IsLinkensProtected(enemyTarget) then
+			if forceStaff and Ability.IsReady(forceStaff) and Ability.IsCastable(forceStaff, myMana) then
+				Ability.CastTarget(forceStaff, enemyTarget) 
+			return end
+			
+			if hurricanePike and Ability.IsReady(hurricanePike) and Ability.IsCastable(hurricanePike, myMana) then
+				Ability.CastTarget(hurricanePike, enemyTarget) 
+			return end
+			
+			if eul and Ability.IsReady(eul) and Ability.IsCastable(eul, myMana) then
+				Ability.CastTarget(eul, enemyTarget) 
+			return end
+			
+			if diffusal and Ability.IsReady(diffusal) and Ability.IsCastable(diffusal, myMana) then
+				Ability.CastTarget(diffusal, enemyTarget) 
+			return end
+			
+			if abyssalblade and Ability.IsReady(abyssalblade) and Ability.IsCastable(abyssalblade, myMana) then
+				Ability.CastTarget(abyssalblade, enemyTarget) 
+			return end
+			
+			if orchid and Ability.IsReady(orchid) and Ability.IsCastable(orchid, myMana) then
+				Ability.CastTarget(orchid, enemyTarget) 
+			return end
+		
+			if bloodthorn and Ability.IsReady(bloodthorn) and Ability.IsCastable(bloodthorn, myMana) then
+				Ability.CastTarget(bloodthorn, enemyTarget) 
+			return end
+			
+			return 
+		end
+		
+		if Blink and Ability.IsCastable(Blink, myMana) and enemyTarget ~= nil and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(enemyTarget),1200,0) then 
+		local enemyAbsOrigin = Entity.GetAbsOrigin(enemyTarget)
+		Ability.CastPosition(Blink,enemyAbsOrigin) 
+		return end
+		
+		if enemyTarget and BKB and Ability.IsReady(BKB) and Ability.IsCastable(BKB, myMana) then
+			Ability.CastNoTarget(BKB) 
+		return end
+		
+		if enemyTarget and lotus and Ability.IsReady(lotus) and Ability.IsCastable(lotus, myMana) then
+			Ability.CastTarget(lotus, myHero) 
+		return end
+		
+		if enemyTarget and manta and Ability.IsReady(manta) and Ability.IsCastable(manta, myMana) then
+			Ability.CastNoTarget(manta) 
+		return end
+		
+		if enemyTarget and phantasm and Ability.IsReady(phantasm) and Ability.IsCastable(phantasm, myMana) then
+			Ability.CastNoTarget(phantasm)
+		return end
+		
+		if enemyTarget and mjolnir and Ability.IsReady(mjolnir) and Ability.IsCastable(mjolnir, myMana) then
+			Ability.CastTarget(mjolnir, myHero) 
+		return end
+		
+		if enemyTarget and rift and Ability.IsReady(rift) and Ability.IsCastable(rift, myMana) then
+			Ability.CastNoTarget(rift, enemyTarget)
+		return end
+		
+		if enemyTarget and bolt and Ability.IsReady(bolt) and Ability.IsCastable(bolt, myMana) then 
+			Ability.CastTarget(bolt, enemyTarget) 
+		return end
+		
+		
+		if enemyTarget and abyssalblade and Ability.IsReady(abyssalblade) and Ability.IsCastable(abyssalblade, myMana) then
+			Ability.CastTarget(abyssalblade, enemyTarget) 
+		return end
+		
+		if enemyTarget and diffusal and Ability.IsReady(diffusal) and Ability.IsCastable(diffusal, myMana) then
+			Ability.CastTarget(diffusal, enemyTarget) 
+		return end
+		
+		if enemyTarget and blademail and Ability.IsReady(blademail) and Ability.IsCastable(blademail, myMana) then
+			Ability.CastNoTarget(blademail) 
+		return end
+		
+		if enemyTarget and medallion and Ability.IsReady(medallion) and Ability.IsCastable(medallion, myMana) then
+			Ability.CastTarget(medallion, enemyTarget) 
+		return end
+		
+		if enemyTarget and solarCrest and Ability.IsReady(solarCrest) and Ability.IsCastable(solarCrest, myMana) then
+			Ability.CastTarget(solarCrest, enemyTarget) 
+		return end
+		
+		if enemyTarget and orchid and Ability.IsReady(orchid) and Ability.IsCastable(orchid, myMana) then
+			Ability.CastTarget(orchid, enemyTarget) 
+		return end
+		
+		if enemyTarget and bloodthorn and Ability.IsReady(bloodthorn) and Ability.IsCastable(bloodthorn, myMana) then
+			Ability.CastTarget(bloodthorn, enemyTarget) 
+		return end
+		
+		if enemyTarget and nullifier and Ability.IsReady(nullifier) and Ability.IsCastable(nullifier, myMana) then
+			Ability.CastTarget(nullifier, enemyTarget) 
+		return end
+		
+		if enemyTarget and MoM and Ability.IsReady(MoM) and Ability.IsCastable(MoM, myMana) then
+			Ability.CastNoTarget(MoM) 
+		return end
+		
+	end
+	
+end
+
+function chaos.OnDraw()
+	if not Menu.IsEnabled(optionAwareness) then return end
+
+    local myHero = Heroes.GetLocal()
+    if not myHero or NPC.GetUnitName(myHero) ~= "npc_dota_hero_chaos_knight" then return end
+
+    local edge_damage = 0
+    local edge = NPC.GetAbility(myHero, "chaos_knight_reality_rift")
+    if edge and Ability.IsCastable(edge, NPC.GetMana(myHero)) then
+    local  edge_damage = NPC.GetTrueDamage(myHero)
+    end
+
+    for i = 1, Heroes.Count() do
+        local enemy = Heroes.Get(i)
+        if not NPC.IsIllusion(enemy) and not Entity.IsSameTeam(myHero, enemy) and not Entity.IsDormant(enemy) and Entity.IsAlive(enemy) then
+            
+            local enemyHp = Entity.GetHealth(enemy)
+            local physical_damage = NPC.GetTrueDamage(myHero) * NPC.GetArmorDamageMultiplier(enemy) 
+            local magical_damage = edge_damage * NPC.GetMagicalArmorDamageMultiplier(enemy)
+            local enemyHpLeft = enemyHp - magical_damage
+            local hitsLeft = math.ceil(enemyHpLeft / math.max(physical_damage, 1))
+            
+            -- Прорисовка
+            local pos = Entity.GetAbsOrigin(enemy)
+            local x, y, visible = Renderer.WorldToScreen(pos)
+
+              -- red : can kill; green : cant kill
+            if enemyHpLeft <= 0 then
+                Renderer.SetDrawColor(255, 0, 0, 255)
+                Renderer.DrawTextCentered(font, x, y, "Kill", 1)
+            else
+                Renderer.SetDrawColor(0, 255, 0, 255)
+                Renderer.DrawTextCentered(font, x, y, hitsLeft, 1)
+            end
+        end
+	end
+end
+
+function chaos.KillSteal(myHero)
+    local rift = NPC.GetAbility(myHero, "chaos_knight_reality_rift")
+    if not rift or not Ability.IsCastable(rift, NPC.GetMana(myHero)) then return end
+    local damage = NPC.GetTrueDamage(myHero)
+
+    local range = 550
+    local enemies = NPC.GetHeroesInRadius(myHero, range, Enum.TeamType.TEAM_ENEMY)
+    if not enemies or #enemies <= 0 then return end
+
+    for i, enemy in ipairs(enemies) do
+    	local true_damage = damage * NPC.GetArmorDamageMultiplier(enemy)
+	    if Entity.GetHealth(enemy) <= true_damage and Utility.CanCastSpellOn(enemy) then
+	        Ability.CastTarget(rift, enemy)
+	        return
+	    end
+    end
+end
+
+return chaos
