b230721838416a4c9d4a423ce00ef26863a86b30
GoldimoR <waldemar1995i@gmail.com>
2018-12-11 18:39:43 +0200

    Update SpiritBreaker.lua

--- SpiritBreaker.lua
+++ SpiritBreaker.lua
@@ -186,6 +186,11 @@ function Bara.OnUpdate()
 			Ability.CastNoTarget(MoM) 
 		return end
 		
+		if enemyTarget ~= nil and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(enemyTarget),1300,0) then 
+		local enemyAbsOrigin = Entity.GetAbsOrigin(enemyTarget)
+		Player.AttackTarget(myPlayer, myHero, enemyTarget)
+		return end
+
 	end
 	
 end
