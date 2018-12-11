7803ead35d321e8b87c574768da472ad5fb540aa
GoldimoR <waldemar1995i@gmail.com>
2018-12-11 18:29:52 +0200

    Update Omniknight.lua

--- Omniknight.lua
+++ Omniknight.lua
@@ -196,6 +196,11 @@ function Omniknight.OnUpdate()
 		Ability.CastTarget(purification, myHero) 
 		return end
 	
+		if enemyTarget ~= nil and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(enemyTarget),1300,0) then 
+		local enemyAbsOrigin = Entity.GetAbsOrigin(enemyTarget)
+		Player.AttackTarget(myPlayer, myHero, enemyTarget)
+		return end
+
     end
 	
 end
