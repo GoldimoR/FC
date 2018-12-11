6ff9d180cd8c9d7c056a38bcfe0424f693cd1d81
GoldimoR <waldemar1995i@gmail.com>
2018-12-11 17:43:58 +0200

    Update Alchemist.lua

--- Alchemist.lua
+++ Alchemist.lua
@@ -190,6 +190,11 @@ function Alchemist.OnUpdate()
 			Ability.CastNoTarget(manta) 
 		return end
 		
+		if enemyTarget ~= nil and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(enemyTarget),1300,0) then 
+		local enemyAbsOrigin = Entity.GetAbsOrigin(enemyTarget)
+		Player.AttackTarget(myPlayer, myHero, enemyTarget)
+		return end
+
 	end
 	
 end
