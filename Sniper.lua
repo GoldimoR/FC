8cebe8252366ad4600a8ee3109f4cee1f9aeb9b1
GoldimoR <waldemar1995i@gmail.com>
2018-12-11 18:38:15 +0200

    Update Sniper.lua

--- Sniper.lua
+++ Sniper.lua
@@ -179,6 +179,11 @@ function Sniper.OnUpdate()
 			Ability.CastNoTarget(MoM) 
 		return end
 		
+		if enemyTarget ~= nil and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(enemyTarget),1300,0) then 
+		local enemyAbsOrigin = Entity.GetAbsOrigin(enemyTarget)
+		Player.AttackTarget(myPlayer, myHero, enemyTarget)
+		return end
+
 	end
 	
 end
